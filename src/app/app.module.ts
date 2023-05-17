97599ad52658f99f9ece6a83a3a4fb9c5395ddd1
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-05-17 23:29:11 +0530

    Init app with tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SeatsComponent } from './components/seats/seats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeatsComponent
   ],
   imports: [
     BrowserModule
